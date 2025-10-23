579c461bdaa80ae4eb40f75e39fb5e8d21afbcac
Nicolas Dujacquier <nicolas.dujacquier@nbb.be>
2025-10-23 15:33:10 +0200

    feat(stark-ui): Replace `@mdi/angular-material` icons provider by `@nationalbankbelgium/mdi-ts`
    In stark-ui we now use icons provide in `TypeScript` by `@nationalbankbelgium/mdi-ts`.
    Those icon are provide in TypeScript classes an we can detect when an icon is not available anymore.

---
+++ packages/stark-ui/src/modules/ts-icons/ts-icons.module.ts
@@ -0,0 +1,33 @@
+import { Inject, InjectionToken, ModuleWithProviders, NgModule, makeEnvironmentProviders } from "@angular/core";
+import { MatIconModule, MatIconRegistry } from "@angular/material/icon";
+import { DomSanitizer } from "@angular/platform-browser";
+import { mdiIcon } from "@nationalbankbelgium/mdi-ts";
+
+export const ICONS_PROVIDER = new InjectionToken<mdiIcon[][]>("iconsProviders");
+
+@NgModule({
+	imports: [MatIconModule]
+})
+export class TsIconsModule {
+	public constructor(iconRegistry: MatIconRegistry, domSanitizer: DomSanitizer, @Inject(ICONS_PROVIDER) iconsToInject: mdiIcon[][]) {
+		iconsToInject.forEach((icons) => {
+			icons.forEach((icon) => {
+				iconRegistry.addSvgIconLiteral(icon.name, domSanitizer.bypassSecurityTrustHtml(icon.data));
+			});
+		});
+	}
+
+	public static forRoot(mdiIcons: mdiIcon[]): ModuleWithProviders<TsIconsModule> {
+		return {
+			ngModule: TsIconsModule,
+			providers: [makeEnvironmentProviders([{ provide: ICONS_PROVIDER, useValue: mdiIcons, multi: true }])]
+		};
+	}
+
+	public static forChild(mdiIcons: mdiIcon[]): ModuleWithProviders<TsIconsModule> {
+		return {
+			ngModule: TsIconsModule,
+			providers: [makeEnvironmentProviders([{ provide: ICONS_PROVIDER, useValue: mdiIcons, multi: true }])]
+		};
+	}
+}
